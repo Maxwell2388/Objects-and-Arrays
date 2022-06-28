b60f378b2cd539c00a1ef4cb9561c91ecb28ad68
Maxtech <101644202+Maxwell2388@users.noreply.github.com>
2022-06-28 15:48:38 -0700

    main.js file for Objects and Arrays
    Techcampus task on objects and Arrays

---
+++ main.js
@@ -0,0 +1,102 @@
+const data_workers = [
+        {person_1: ["1.", "Thompson", "Anthony", "32", "Male", "Head of Finance Department", "17/07/2014"]},
+        {person_2: ["2.", "Johnson", "Mary", "23", "Female", "Software Engineer", "05/03/2019"]},
+        {person_3: ["3", "Chisom", "Onyeka", "28", "Female", "Human Resource manager", "21/06/2016"]},
+        {person_4: ["4", "Jide", "Olabisi", "29", "Male", "Assistant Head(Software Department", "05/06/2016"]},
+        {person_5: ["5", "Williams", "Ajayi", "30", "Male", "Head of Software Department", "06/09/2015"]},
+        {person_6: ["6", "Nancy", "Chukwuemeka", "27", "Female", "Secretary", "23/08/2016"]},
+        {person_7: ["7", "Nicholas", "Uchemba", "26", "Male", "Software Engineer", "22/02/2020"]},
+        {person_8: ["8", "Ahmed", "Olayinka", "24", "Male", "Graphic Designer", "15/08/2020"]},
+        {person_9: ["9", "Halima", "Mohammed", "26", "Female", "Senior Finance Manager", "30/05/2018"]},
+        {person_10: ["10", "Richard", "Adeyemi", "41", "Male", "C.E.O", "17/11/2010"]},
+
+]
+
+document.getElementById("p1").innerHTML = data_workers[0].person_1[0]
+document.getElementById("p2").innerHTML = data_workers[0].person_1[1]
+document.getElementById("p3").innerHTML = data_workers[0].person_1[2]
+document.getElementById("p4").innerHTML = data_workers[0].person_1[3]
+document.getElementById("p5").innerHTML = data_workers[0].person_1[4]
+document.getElementById("p6").innerHTML = data_workers[0].person_1[5]
+document.getElementById("p7").innerHTML = data_workers[0].person_1[6]
+
+document.getElementById("v1").innerHTML = data_workers[1].person_2[0]
+document.getElementById("v2").innerHTML = data_workers[1].person_2[1]
+document.getElementById("v3").innerHTML = data_workers[1].person_2[2]
+document.getElementById("v4").innerHTML = data_workers[1].person_2[3]
+document.getElementById("v5").innerHTML = data_workers[1].person_2[4]
+document.getElementById("v6").innerHTML = data_workers[1].person_2[5]
+document.getElementById("v7").innerHTML = data_workers[1].person_2[6]
+
+
+document.getElementById("t1").innerHTML = data_workers[2].person_3[0]
+document.getElementById("t2").innerHTML = data_workers[2].person_3[1]
+document.getElementById("t3").innerHTML = data_workers[2].person_3[2]
+document.getElementById("t4").innerHTML = data_workers[2].person_3[3]
+document.getElementById("t5").innerHTML = data_workers[2].person_3[4]
+document.getElementById("t6").innerHTML = data_workers[2].person_3[5]
+document.getElementById("t7").innerHTML = data_workers[2].person_3[6]
+
+document.getElementById("b1").innerHTML = data_workers[3].person_4[0]
+document.getElementById("b2").innerHTML = data_workers[3].person_4[1]
+document.getElementById("b3").innerHTML = data_workers[3].person_4[2]
+document.getElementById("b4").innerHTML = data_workers[3].person_4[3]
+document.getElementById("b5").innerHTML = data_workers[3].person_4[4]
+document.getElementById("b6").innerHTML = data_workers[3].person_4[5]
+document.getElementById("b7").innerHTML = data_workers[3].person_4[6]
+
+document.getElementById("c1").innerHTML = data_workers[4].person_5[0]
+document.getElementById("c2").innerHTML = data_workers[4].person_5[1]
+document.getElementById("c3").innerHTML = data_workers[4].person_5[2]
+document.getElementById("c4").innerHTML = data_workers[4].person_5[3]
+document.getElementById("c5").innerHTML = data_workers[4].person_5[4]
+document.getElementById("c6").innerHTML = data_workers[4].person_5[5]
+document.getElementById("c7").innerHTML = data_workers[4].person_5[6]
+
+document.getElementById("x1").innerHTML = data_workers[5].person_6[0]
+document.getElementById("x2").innerHTML = data_workers[5].person_6[1]
+document.getElementById("x3").innerHTML = data_workers[5].person_6[2]
+document.getElementById("x4").innerHTML = data_workers[5].person_6[3]
+document.getElementById("x5").innerHTML = data_workers[5].person_6[4]
+document.getElementById("x6").innerHTML = data_workers[5].person_6[5]
+document.getElementById("x7").innerHTML = data_workers[5].person_6[6]
+
+document.getElementById("y1").innerHTML = data_workers[6].person_7[0]
+document.getElementById("y2").innerHTML = data_workers[6].person_7[1]
+document.getElementById("y3").innerHTML = data_workers[6].person_7[2]
+document.getElementById("y4").innerHTML = data_workers[6].person_7[3]
+document.getElementById("y5").innerHTML = data_workers[6].person_7[4]
+document.getElementById("y6").innerHTML = data_workers[6].person_7[5]
+document.getElementById("y7").innerHTML = data_workers[6].person_7[6]
+
+
+document.getElementById("z1").innerHTML = data_workers[7].person_8[0]
+document.getElementById("z2").innerHTML = data_workers[7].person_8[1]
+document.getElementById("z3").innerHTML = data_workers[7].person_8[2]
+document.getElementById("z4").innerHTML = data_workers[7].person_8[3]
+document.getElementById("z5").innerHTML = data_workers[7].person_8[4]
+document.getElementById("z6").innerHTML = data_workers[7].person_8[5]
+document.getElementById("z7").innerHTML = data_workers[7].person_8[6]
+
+document.getElementById("j1").innerHTML = data_workers[8].person_9[0]
+document.getElementById("j2").innerHTML = data_workers[8].person_9[1]
+document.getElementById("j3").innerHTML = data_workers[8].person_9[2]
+document.getElementById("j4").innerHTML = data_workers[8].person_9[3]
+document.getElementById("j5").innerHTML = data_workers[8].person_9[4]
+document.getElementById("j6").innerHTML = data_workers[8].person_9[5]
+document.getElementById("j7").innerHTML = data_workers[8].person_9[6]
+
+document.getElementById("k1").innerHTML = data_workers[9].person_10[0]
+document.getElementById("k2").innerHTML = data_workers[9].person_10[1]
+document.getElementById("k3").innerHTML = data_workers[9].person_10[2]
+document.getElementById("k4").innerHTML = data_workers[9].person_10[3]
+document.getElementById("k5").innerHTML = data_workers[9].person_10[4]
+document.getElementById("k6").innerHTML = data_workers[9].person_10[5]
+document.getElementById("k7").innerHTML = data_workers[9].person_10[6]
+
+
+
+
+
+
+
